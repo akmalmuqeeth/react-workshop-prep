36ffb8152431bdcfcb068de0e3e4296f853fb471
akmal <akmal@brighter.com>
2017-05-12 06:34:37 -0700

    split code into modules and use webpack to bundle everything together

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
+var titleFactory = React.createFactory(MyTitle);
+
 var div = React.DOM.div;
-var h1 = React.DOM.h1;
-
-var titleComponent = React.createClass({
-	render : function(){
-		return (
-			div(null,
-				h1({style : {color : this.props.color}}, this.props.title)
-			)
-		)
-	}
-});
-
-var titleFactory = React.createFactory(titleComponent);
 
 
 var mainComponent = React.createClass({
