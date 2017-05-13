18d2cc4781cc0bdbe4d1ab3992ab39601cc7083f
akmal <akmal@brighter.com>
2017-05-12 21:05:42 -0700

    use myTile component to render

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var TitleFactory = React.createFactory(MyTitle);
-
-var div = React.DOM.div;
-
-
 var mainComponent = React.createClass({
 	render : function(){
 		return (
 			<div>
-				<TitleFactory title="one" color="red"/>
-				<TitleFactory title="two" color="yellow"/>
-				<TitleFactory title="three" color="blue"/>
+				<MyTitle title="one" color="red"/>
+				<MyTitle title="two" color="yellow"/>
+				<MyTitle title="three" color="blue"/>
 			</div>
 		)
 	}
